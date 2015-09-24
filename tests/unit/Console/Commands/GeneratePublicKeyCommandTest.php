f09794aa696a6c641e73e1abf2f5cbca5bdc004f
Thomas Kerin <afk11@users.noreply.github.com>
2015-09-24 12:44:18 +0100

    Initial commit of console

---
+++ tests/unit/Console/Commands/GeneratePublicKeyCommandTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Mdanter\Ecc\Tests\Console\Commands;
+
+use Mdanter\Ecc\Console\Commands\GeneratePublicKeyCommand;
+use Mdanter\Ecc\Math\MathAdapterFactory;
+use Mdanter\Ecc\Math\MathAdapterInterface;
+use Mdanter\Ecc\Tests\AbstractTestCase;
+
+class GeneratePublicKeyCommandTest extends AbstractCommandTest
+{
+    /**
+     * @dataProvider getAdapters
+     */
+    public function testOutputIsCompatibleWithOpenSSLOutput(MathAdapterInterface $adapter)
+    {
+        MathAdapterFactory::forceAdapter($adapter);
+
+        $commandTester = $this->getCommandTester(new GeneratePublicKeyCommand(), 'encode-pubkey');
+
+        $data = file_get_contents(__DIR__.'/../../../data/openssl-priv.key');
+        $expected = file_get_contents(__DIR__.'/../../../data/openssl-pub.key');
+
+        $commandTester->execute(array('data' => $data));
+
+        $this->assertEquals($this->normalize($expected), $this->normalize($commandTester->getDisplay()));
+
+        MathAdapterFactory::forceAdapter(null);
+    }
+}
